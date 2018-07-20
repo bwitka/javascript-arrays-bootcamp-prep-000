8282ae4e65deda79a919761118cae8af2383a3cc
BEAU WITKA <bwitka@gmail.com>
2018-07-20 05:59:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -41,7 +41,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(-1);
+  var newArray = array.slice(-1);
   return array;
 }
 
